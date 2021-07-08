20444c578448252fbb7026a41cbab42172fe7d75
Nishkal <nishkal8463@gmail.com>
2021-07-08 12:13:01 +0530

    form logic setup and POST API setup

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,11 +10,21 @@ const getPosts = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  const post = req.body;
-  const newPost = new PostMessage(post);
+  console.log("ERERERERERER",req.body)
+  const { title, message, selectedFile, creator, tags } = req.body;
+
+  const newPostMessage = new PostMessage({
+    title,
+    message,
+    selectedFile,
+    creator,
+    tags,
+  });
+
   try {
-    await newPost.save();
-    res.status(201).json(newPost);
+    await newPostMessage.save();
+
+    res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ require('dotenv').config()
 const postRoutes =  require('./routes/posts');
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 app.use('/posts', postRoutes)
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
 
 const CONNECTION_URL = process.env.DB_CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
